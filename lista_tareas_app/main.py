09df6931c024d7bdad164e8bbe8361479320f549
Joselyn Sigcha <ja.sigchaq@uea.edu.ec>
2026-03-28 21:58:31 -0500

    📝 Tarea S15 - Actualización del archivo principal de la Aplicación GUI Lista de Tareas✍️

--- lista_tareas_app/main.py
+++ lista_tareas_app/main.py
@@ -1,21 +1,25 @@
 """
 Aplicación de escritorio tipo To-Do List desarrollada en Python con Tkinter,
 aplicando Programación Orientada a Objetos (POO), arquitectura modular por capas
-y manejo de eventos de usuario (teclado y ratón).
+y manejo de eventos de usuario (teclado para agregar tareas y ratón para interactuar).
+Esta aplicación permite la gestión de tareas al permitir agregar, completar y eliminar
+actividades de manera sencilla e intuitiva, proporcionando una experiencia de usuario
+fluida y eficiente.
 """
 
+# Importa la clase de servicio y la interfaz gráfica
 from servicios.tarea_servicio import TareaServicio
 from ui.app_tkinter import AppTkinter
 
-
+# Función principal para iniciar la aplicación
 def main():
     # Instancia la capa de servicio
     servicio = TareaServicio()
-    #Construye la UI inyectando el servicio.
+    # Construye la UI inyectando el servicio.
     app = AppTkinter(servicio)
-    # Arrancar el bucle de eventos a través del único método público
+    # Inicia la aplicación
     app.run()
 
-
+# Punto de entrada del programa, que llama a la función main para iniciar el sistema
 if __name__ == "__main__":
     main()
