c55cde7959c90f48b42b99b872727aac641895fb
Joselyn Sigcha <ja.sigchaq@uea.edu.ec>
2026-03-28 19:23:54 -0500

    📝 Tarea S15 - Actualización con comentarios de la clase tarea de la carpeta modelos de la Aplicación GUI Lista de Tareas✍️

--- lista_tareas_app/modelos/tarea.py
+++ lista_tareas_app/modelos/tarea.py
@@ -1,33 +1,37 @@
+# Clase que representa una tarea del sistema
 class Tarea:
-
+    
+    # Constructor de la clase con datos y su tipo
     def __init__(self, id_tarea: int, descripcion: str):
-        self.__id = id_tarea
-        self.__descripcion = descripcion.strip()
-        self.__completada = False
+        self._id = id_tarea # Atributo privado del identificador único de la tarea
+        self._descripcion = descripcion.strip() # Atributo privado de la descripción de la tarea, sin espacios al inicio o final
+        self._completada = False # Atributo privado que indica si la tarea está completada (inicialmente en False)
 
-    # GETTERS
+    # Métodos GETTERS que permiten acceder a los atributos privados de forma controlada
     @property
     def id(self):
-        return self.__id
+        return self._id
 
     @property
     def descripcion(self):
-        return self.__descripcion
+        return self._descripcion
 
     @property
     def completada(self):
-        return self.__completada
+        return self._completada
 
-    # SETTER
+    # Método SETTER que permiten modificar la descripción o tarea de forma controlada en caso de requerir una actualización
     @descripcion.setter
     def descripcion(self, nueva_desc):
         if not nueva_desc.strip():
-            raise ValueError("La tarea no puede estar vacía.")
-        self.__descripcion = nueva_desc.strip()
-
+            raise ValueError("La descripción de la tarea no puede estar vacía.")
+        self._descripcion = nueva_desc.strip()
+    
+    # Método que marca la tarea como completada, cambiando su estado a True
     def marcar_completada(self):
-        self.__completada = True
+        self._completada = True
 
+    # Método que devuelve la información de la tarea
     def __repr__(self):
         estado = "✅" if self.completada else "❌"
-        return f"[{estado}] ({self.__id}) {self.__descripcion}"
+        return f"[{estado}] ({self._id}) {self._descripcion}"
